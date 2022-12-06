74521b538554917ef94a75c8cf12613ca8858bce
TylerS2000 <tylerschulten@gmail.com>
2022-12-05 22:51:31 -0500

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+let numbers =[
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    },
+    { 
+        "id": 5,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+]
+
+
+app.get('/info',(req,res)=>{
+    res.send(`Phonebook has info for ${numbers.length} people (request made at ${new Date})`)
+    
+})
+app.get('/api/numbers',(req,res)=>{
+    res.json(numbers);
+})
+
+const PORT = 3001 
+
+app.listen(PORT,()=>{console.log(`Server running on port ${PORT}`)})
